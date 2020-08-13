1b0bf33649776229e8df33cab0531e2811a8d7b7
bodyanba <galkinbogdan@gmail.com>
2020-08-13 12:39:13 +0300

    header and footer

--- js/common.js
+++ js/common.js
@@ -9,50 +9,34 @@ $(document).ready(function() {
   			items: 2
   		},
   		1200: {
-  			items: 2,
-		    nav: true,
-		    dots: false,
-  		},
-  		1500: {
   			items: 3,
-		    nav: true,
-		    dots: false,
   		},
   	},
-    items: 3,
-    margin: 18,
+    margin: 20,
+    nav: true,
+    dots: false,
     smartSpeed: 1000,
     navText: ['', ''],
     autoHeight: true,
   });
 
   $('.owl-2').owlCarousel({
-  	responsive: {
-  		1200: {
-		    nav: true,
-		    dots: false,
-  		},
-  	},
     items: 1,
-    margin: 15,
+    margin: 20,
+    nav: true,
+    dots: false,
     smartSpeed: 1000,
     navText: ['', ''],
     autoHeight: true,
   });
 
-  $('.video-modal').on('show.bs.modal', function() {
-    var src = 'https://www.youtube.com/embed/OKW018foQmg?autoplay=1';
-    $(this).find('iframe').attr('src', src);
-  }).on('hidden.bs.modal', function() {
-    $(this).find('iframe').removeAttr('src');
-	});
+  $('#search').on('click', function() {
+    $('.header').toggleClass('search-open');
+  });
 
-	$('input[name="options"]').on('change', function() {
-		if ($(this).val() == '1')
-			$('#row').removeClass('active')
-		else 
-			$('#row').addClass('active')
-	});
+  $('#sandwich').on('click', function() {
+    $('body').toggleClass('menubar-in');
+  });
 
 });
 
